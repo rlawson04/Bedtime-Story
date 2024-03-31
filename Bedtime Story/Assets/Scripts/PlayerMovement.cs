37ae5a037c314a50f9bc6308de9cdaefeb4051e9
rlawson04 <rbl2991@rit.edu>
2024-03-31 02:08:51 -0400

    Player Movement

--- Bedtime Story/Assets/Scripts/PlayerMovement.cs
+++ Bedtime Story/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] CharacterController2D characterController;
+
+    [SerializeField] float speed;
+    float horizontalMove = 0f;
+    bool isJumping = false;
+    bool isCrouching = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;    
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            isCrouching = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            isCrouching= false;
+        }
+    }
+
+    /// <summary>
+    /// Using fixed update to move the player  since it is not advised to move inside update
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Move the character
+        characterController.Move(horizontalMove * Time.fixedDeltaTime, isCrouching, isJumping);
+        isJumping = false;
     }
 }
